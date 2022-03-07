6ac73f8ec4be923b7700bc3fcd08f138c9d78358
Jeremy L. Shepherd <jeremy.shepherd@lexisnexis.com>
2022-03-07 17:44:53 -0500

    Basic App structure, Model, FIrst resource

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('News'),
+        ),
+        body: const Center(
+          child: Text('News'),
+        ),
+      ),
+    );
+  }
+}
